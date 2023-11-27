5740ef96173b2130ad87ed69effba5c7740c2179
L3xi4n <livianedelea@gmail.com>
2023-11-27 23:14:15 +0100

    .

--- 5_S/L5/assets/js/medium.js
+++ 5_S/L5/assets/js/medium.js
@@ -1,1 +1,15 @@
-// if(window.self !== window.top) window.location = "about:blank"
+// const navbar = document.getElementById('navbar-fixed');
+
+// const navbarHeightPx = 50;
+
+// window.addEventListener('scroll', () => {
+//   if (
+//     document.body.scrollTop > navbarHeightPx ||
+//     document.documentElement.scrollTop > navbarHeightPx
+//   ) {
+//     navbar.classList.add('scrolled');
+//   } else {
+//     navbar.classList.remove('scrolled');
+//   }
+// });
+
