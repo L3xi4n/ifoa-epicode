2eafba0d2c2b5331add6dcb75a6c4c7cc3c24f5b
L3xi4n <livianedelea@gmail.com>
2023-12-05 17:27:43 +0100

    S_7 es_1 L1

---
+++ 7_S/L1/assets/js/index.js
@@ -0,0 +1,60 @@
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  greetings() {
+    // console.log(`I am ${this.age} years old`);
+
+    return (
+      "hello, my name is " +
+      this.firstName +
+      " " +
+      this.lastName +
+      " I am " +
+      this.age +
+      " years old and I live in " +
+      this.location
+    );
+  }
+}
+
+class NewAge extends User {
+  constructor(age) {
+    super(age);
+    this.age = age;
+    if (user1.age > user2.age) {
+      console.log(
+        user1.firstName +
+          " is " +
+          user1.age +
+          " and is older than " +
+          user2.firstName +
+          " that is " +
+          user2.age
+      );
+    } else {
+      console.log(
+        user2.firstName +
+          " is " +
+          user2.age +
+          " and is older than " +
+          user1.firstName +
+          " that is " +
+          user1.age
+      );
+    }
+  }
+  static displayAge = NewAge;
+}
+
+const user1 = new User("John", "Doe", "25", "New York");
+const user2 = new User("Linda", "Smith", "30", "Mexico");
+
+console.log(user1.greetings());
+console.log(user2.greetings());
+
+const age = new NewAge(user1.age, user2.age);
